c144bf30e6cf3541c3d99418193f0f474068d3fe
Bartosz Picheta <b.picheta@nowaera.pl>
2020-02-09 23:43:58 +0100

    navbar and two main components ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent
   ],
   imports: [
+    RouterModule,
+    CommonModule,
     BrowserModule,
     AppRoutingModule
   ],
